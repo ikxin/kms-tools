214bf8a84f5ac1ec267296f91b7dcbc5e9af866f
一纸忘忧 <i@ikxin.com>
2022-05-19 23:28:07 +0800

    feat: 创建Header组件

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-layout-header>
+    <div class="logo">
+      <img src="../assets/logo.svg">
+    </div>
+    <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    return {
+      selectedKeys: ref(['1']),
+    };
+  },
+
+});
+</script>
+
+<style lang="less">
+.logo {
+  float: left;
+  width: 120px;
+  text-align: center;
+
+  img {
+    height: 36px;
+  }
+}
+</style>
