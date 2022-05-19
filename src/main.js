ac324a65b06cfbea0d6f6db809c03e62ed693c65
一纸忘忧 <i@ikxin.com>
2022-05-19 21:52:50 +0800

    feat: 引入ant-design-vue组件库

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入ant-design-vue
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Antd)
+app.mount('#app')
