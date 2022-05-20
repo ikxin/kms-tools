614e5afb6d6f4878dbf1016e927637e47dd0ddae
一纸忘忧 <i@ikxin.com>
2022-05-20 11:20:01 +0800

    style: 更新Header组件样式

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
   <a-layout-header>
-    <div class="logo">
-      <img src="../assets/images/logo.svg">
+    <div class="wrapper">
+      <div class="logo">
+        <img src="../assets/images/logo.svg">
+      </div>
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="horizontal"
+        :style="{ lineHeight: '64px', float: 'right' }">
+        <a-menu-item key="1">Blog</a-menu-item>
+        <a-menu-item key="2">Github</a-menu-item>
+      </a-menu>
     </div>
-    <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
-    </a-menu>
+
   </a-layout-header>
 </template>
 
@@ -16,21 +19,30 @@ import { defineComponent, ref } from 'vue';
 export default defineComponent({
   setup() {
     return {
-      selectedKeys: ref(['1']),
+      selectedKeys: ref(['2']),
     };
   },
-
 });
 </script>
 
-<style lang="less">
-.logo {
-  float: left;
-  width: 120px;
-  text-align: center;
+<style lang="less" scoped>
+.ant-layout-header {
+  padding: 0;
+}
 
-  img {
-    height: 36px;
+.wrapper {
+  width: 1080px;
+  max-width: 100%;
+  margin: 0 auto;
+
+  .logo {
+    float: left;
+    width: 120px;
+    text-align: center;
+
+    img {
+      height: 36px;
+    }
   }
 }
 </style>
