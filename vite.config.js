707338701c9907460d2942ef9230720e56a584a5
一纸忘忧 <i@ikxin.com>
2022-05-27 15:56:19 +0800

    build: 调整chunk大小警告的限制
    (!) Some chunks are larger than 500 KiB after minification. Consider:
    - Using dynamic import() to code-split the application
    - Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/guide/en/#outputmanualchunks
    - Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    // chunk 大小警告的限制（以 kbs 为单位）
+    chunkSizeWarningLimit: 2000
+  }
 })
