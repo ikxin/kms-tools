d13a56e4f9dee66ed5db2d21ca08ec3fbdf25098
一纸忘忧 <i@ikxin.com>
2022-06-04 16:06:14 +0800

    feat: 新增侧边栏

--- src/views/activate/index.vue
+++ src/views/activate/index.vue
@@ -1,12 +1,29 @@
 <template>
   <a-layout>
-    <a-layout-sider width="200" style="background: #fff"></a-layout-sider>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu openKeys="sub1" mode="inline" style="height: 100%">
+        <a-sub-menu key="sub1">
+          <template #title>
+            <icon-park type="system" theme="outline" />
+            <span>系统类型</span>
+          </template>
+          <a-menu-item key="1">
+            <icon-park type="windows" theme="outline" />
+            <span>Windows</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <icon-park type="word" theme="outline" />
+            <span>Office</span>
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
     <router-view />
   </a-layout>
 </template>
 
 <script setup>
-
+import { IconPark } from '@icon-park/vue-next/es/all'
 </script>
 
 <style lang="less" scoped>
@@ -16,5 +33,18 @@
 
 .ant-layout-sider {
   margin-right: 20px;
+}
+
+:deep(.ant-menu-title-content) {
+  display: flex;
+  align-items: center;
+
+  span {
+    line-height: 0;
+  }
+
+  span:first-child {
+    margin-right: 8px;
+  }
 }
 </style>
