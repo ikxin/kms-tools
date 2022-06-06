1477372e2a7f98c5a50303a11d9885d66f515697
一纸忘忧 <i@ikxin.com>
2022-06-06 09:35:15 +0800

    refactor(路由): 修改路由history为WebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     {
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 })
 
