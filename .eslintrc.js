d027017cc5910bbf50f3e064f96efd74b65da24a
一纸忘忧 <i@ikxin.com>
2022-06-07 15:48:42 +0800

    style: 新增eslint规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,12 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'standard'
-  ],
+  extends: ['plugin:vue/essential', 'standard', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
 }
