97ee2c0ea14f9c546fece6cd9cb9f51713b03d92
一纸忘忧 <i@ikxin.com>
2022-06-30 09:56:47 +0800

    feat: 新增Office激活

--- src/views/activation/office.vue
+++ src/views/activation/office.vue
@@ -1,46 +1,41 @@
 <template>
   <a-layout>
-    <!-- 使用说明 -->
-    <readmeVue>
-      <template #explain>
-        <p>1. 在桌面右键选择此电脑点击属性，查看当前电脑的系统版本</p>
-        <p>2. 在下方表单中选择对应的系统版本，复制或下载激活脚本，使用管理员权限运行该脚本。</p>
-        <p>3. 使用KMS服务器激活系统后，有效期为180天。</p>
-        <p>4. 系统每7天会连接一次KMS服务器，获取最新的授权，然后激活有效期会重置为180天。</p>
-        <p>5. 如果激活失败可先尝试清除后激活，点击下载<a @click="downloadCleanScript">清除脚本</a>。</p>
-      </template>
-    </readmeVue>
-
-    <!-- 选择系统 -->
+    <!-- 选择版本 -->
     <a-card>
-      <a-form>
-        <a-form-item label="系统类型" name="type">
-          <a-select v-model:value="form.type" :allowClear="true" placeholder="请选择系统类型">
+      <a-form :labelCol="{ span: 3 }">
+        <a-form-item label="Office版本" name="officeVersion">
+          <a-select v-model:value="params.officeVersion" :allowClear="true" placeholder="请选择系统类型">
             <a-select-option v-for="item in officeData" :key="item.id" :value="item.id">
               {{ item.version }}
             </a-select-option>
           </a-select>
         </a-form-item>
-        <a-form-item label="激活密钥" name="key">
-          <a-input v-model:value="form.key" disabled />
+        <a-form-item label="系统版本" name="systemVersion">
+          <a-radio-group v-model:value="params.systemVersion">
+            <a-radio-button :value="64">64位</a-radio-button>
+            <a-radio-button :value="32">32位</a-radio-button>
+          </a-radio-group>
         </a-form-item>
-        <a-form-item label="KMS服务器" name="server">
-          <a-input v-model:value="form.server" />
+        <a-form-item label="激活密钥" name="softwareVersionKey">
+          <a-input v-model:value="params.softwareVersion.key" disabled />
+        </a-form-item>
+        <a-form-item label="KMS服务器" name="kmsServer">
+          <a-input v-model:value="params.kmsServer" />
         </a-form-item>
         <a-form-item>
           <a-space size="middle">
             <a-button @click="generateScript">生成脚本</a-button>
             <a-button @click="downloadScript" type="primary">下载脚本</a-button>
-            <a-button v-show="form.script.visible" @click="copyScript" type="primary">复制脚本</a-button>
+            <a-button v-show="params.activationScript.visible" @click="copyScript" type="primary">复制脚本</a-button>
           </a-space>
         </a-form-item>
-        <a-form-item v-show="form.script.visible">
-          <a-textarea v-model:value="form.script.value" :rows="5" />
+        <a-form-item v-show="params.activationScript.visible">
+          <a-textarea v-model:value="params.activationScript.content" :rows="5" />
         </a-form-item>
       </a-form>
     </a-card>
 
-    <!-- 系统版本数据 -->
+    <!-- 版本数据 -->
     <a-card>
       <a-table :dataSource="list.dataSource" :columns="list.columns" :rowSelection="list.rowSelection" rowKey="id" size="middle" />
     </a-card>
@@ -48,23 +43,69 @@
 </template>
 
 <script setup>
-import readmeVue from './components/readme'
 import officeData from '../../data/office.json'
 
 import { message } from 'ant-design-vue'
-import { reactive, watch } from 'vue'
+import { computed, reactive, watch } from 'vue'
 import { useScriptDownload, useScriptCopy } from '../../hooks/script'
 
-const form = reactive({
-  type: undefined,
-  key: '',
-  server: '',
-  script: {
+// 生成脚本参数
+const params = reactive({
+  officeVersion: undefined,
+  systemVersion: 64,
+  softwareVersion: {
+    id: null,
+    name: null,
+    key: null
+  },
+  kmsServer: 'kms.moeclub.org',
+  activationScript: {
     visible: false,
-    value: ''
+    content: null
   }
 })
 
+// 计算激活脚本的内容
+params.activationScript.content = computed(() => {
+  let catalogue
+  if (params.systemVersion === 64) {
+    catalogue = 'cd C:\\Program Files\\Microsoft Office\\Office16'
+  } else {
+    catalogue = 'cd C:\\Program Files (x86)\\Microsoft Office\\Office16'
+  }
+  const fix = `cscript ospp.vbs /inpkey:${params.softwareVersion.key}`
+  const activation = `cscript ospp.vbs /sethst:${params.kmsServer}\r\ncscript ospp.vbs /act`
+  return `${catalogue}\r\n${fix}\r\n${activation}`
+})
+
+// 创建脚本
+function generateScript() {
+  if (params.softwareVersion.key && params.kmsServer) {
+    params.activationScript.visible = true
+  } else {
+    message.error('请填写完整内容')
+  }
+}
+
+// 下载脚本
+function downloadScript() {
+  if (params.softwareVersion.key && params.kmsServer) {
+    useScriptDownload(params.activationScript.content, 'office.bat')
+  } else {
+    message.error('请填写完整内容')
+  }
+}
+
+// 复制脚本
+function copyScript() {
+  if (params.softwareVersion.key && params.kmsServer) {
+    useScriptCopy(params.activationScript.content)
+  } else {
+    message.error('请填写完整内容')
+  }
+}
+
+// 列表属性
 const list = reactive({
   dataSource: [],
   columns: [
@@ -82,55 +123,24 @@ const list = reactive({
   rowSelection: {
     type: 'radio',
     onChange: (_, selectedRows) => {
-      form.key = selectedRows[0].key
+      params.softwareVersion.key = selectedRows[0].key
     }
   }
 })
 
 watch(
-  () => form.type,
+  () => params.officeVersion,
   () => {
-    if (form.type === undefined) {
+    if (params.officeVersion === undefined) {
       list.dataSource = []
     } else {
-      list.dataSource = officeData[form.type].item
+      list.dataSource = officeData[params.officeVersion].item
     }
   },
   {
     immediate: true
   }
 )
-
-function generateScript() {
-  if (form.key && form.server) {
-    form.script = `@echo off\r\nslmgr /skms ${form.server}\r\nslmgr /ipk ${form.key}\r\nslmgr /ato\r\nslmgr /xpr`
-    form.visible = true
-  } else {
-    message.error('未选择系统版本')
-  }
-}
-
-function downloadScript() {
-  if (form.key && form.server) {
-    form.script = `@echo off\r\nslmgr /skms ${form.server}\r\nslmgr /ipk ${form.key}\r\nslmgr /ato\r\nslmgr /xpr`
-    useScriptDownload(form.script, 'kms.bat')
-  } else {
-    message.error('未选择系统版本')
-  }
-}
-
-function downloadCleanScript() {
-  const cleanScript = `@echo off\r\nslmgr /upk\r\nslmgr /ckms\r\nslmgr /rearm`
-  useScriptDownload(cleanScript, 'clean.bat')
-}
-
-function copyScript() {
-  if (form.key && form.server) {
-    useScriptCopy(form.script)
-  } else {
-    message.error('未选择系统版本')
-  }
-}
 </script>
 
 <style lang="less" scoped>
