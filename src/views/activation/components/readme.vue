999441ce00a0f0fd447e85fec22e2199f7ca658c
一纸忘忧 <i@ikxin.com>
2022-07-20 10:35:19 +0800

    refactor: 优化windows脚本生成方式

--- src/views/activation/components/readme.vue
+++ src/views/activation/components/readme.vue
@@ -1,7 +1,7 @@
 <template>
   <a-card title="使用说明">
     <template #extra>
-      <a href="#">more</a>
+      <!-- <a href="#">more</a> -->
     </template>
     <slot name="explain"></slot>
   </a-card>
