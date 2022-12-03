4b9c30d3ae013f910bf26c4592be8f0db1ce8cea
一纸忘忧 <i@ikxin.com>
2022-12-04 00:38:43 +0800

    feat: add type detection for .vue files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,7 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
-declare module 'vue'
