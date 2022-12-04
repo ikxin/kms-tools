b092f68d460d52d47cc27492fa473f65319a55a2
一纸忘忧 <i@ikxin.com>
2022-12-04 14:10:25 +0800

    style: replace ant design vue to arco design vue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
 import 'tailwindcss/utilities.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(Antd)
+app.use(ArcoVue)
 app.mount('#app')
