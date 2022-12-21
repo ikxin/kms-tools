b56130681366a898a9e5a0cf08e9bc01452025db
一纸忘忧 <i@ikxin.com>
2022-12-21 22:04:03 +0800

    style(eslint): close multi word component names

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HeaderLayout from '@/layouts/HeaderLayout.vue'
+import HeaderVue from '@/layouts/header.vue'
 </script>
 
 <template>
   <div class="flex h-screen flex-col bg-[#f0f2f5]">
-    <header-layout></header-layout>
+    <HeaderVue></HeaderVue>
   </div>
 </template>
