513dc3365392ad02e5604ac85f972ba4d30e9448
一纸忘忧 <i@ikxin.com>
2022-12-21 22:33:24 +0800

    feat(footer): add footer layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import HeaderVue from '@/layouts/header.vue'
+import FooterVue from '@/layouts/footer.vue'
 </script>
 
 <template>
   <div class="flex h-screen flex-col bg-[#f0f2f5]">
     <HeaderVue></HeaderVue>
+    <a-layout-content class="grow">
+      <RouterView></RouterView>
+    </a-layout-content>
+    <FooterVue></FooterVue>
   </div>
 </template>
---
+++ src/layouts/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer class="bg-white p-0">
+    <div class="mx-auto flex h-14 w-container max-w-full items-center justify-center whitespace-nowrap">
+      <a-space>
+        <div>
+          Copyright &copy; 2022
+          <a-link target="_blank" href="https://github.com/ikxin/kms-tools">KMS Tools</a-link>
+        </div>
+        <div>
+          Code with by
+          <a-link target="_blank" href="https://www.ikxin.com">一纸忘忧</a-link>
+        </div>
+      </a-space>
+    </div>
+  </a-layout-footer>
+</template>
