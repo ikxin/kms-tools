3c1c34550a6f29716d9e6d9c9ff37c484ba93b3f
一纸忘忧 <i@ikxin.com>
2023-02-17 09:27:26 +0800

    feat: 首页 readme.md 界面

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import README from '../../README.md'
+</script>
 
 <template>
-  <div>HomePage</div>
+  <a-card>
+    <README></README>
+  </a-card>
 </template>
+
+<style>
+img {
+  max-width: 100%;
+}
+</style>
