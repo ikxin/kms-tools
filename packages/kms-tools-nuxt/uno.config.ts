725feaf955e62f758124fbc90f4fe3dd0fc6bd44
一纸忘忧 <i@ikxin.com>
2023-05-30 16:54:58 +0800

    fix: 修复 unocss 在 monorepo 中不生效

--- packages/kms-tools-nuxt/uno.config.ts
+++ packages/kms-tools-nuxt/uno.config.ts
@@ -1,6 +1,9 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno, presetIcons, presetAttributify } from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
   // ...UnoCSS options
+  presets: [presetUno(), presetIcons(), presetAttributify()],
+  transformers: [transformerDirectives()],
 })
