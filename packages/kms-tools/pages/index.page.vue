8f1973ba3c34721ac6662540cd8005419c42d1f4
一纸忘忧 <i@ikxin.com>
2023-05-31 16:39:08 +0800

    feat: 最小化服务端渲染

---
+++ packages/kms-tools/pages/index.page.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Welcome</h1>
+  This page is:
+  <ul>
+    <li>Rendered to HTML.</li>
+    <li>
+      Interactive.
+      <button type="button" @click="state.count++">
+        Counter {{ state.count }}
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+const state = reactive({ count: 0 })
+</script>
---
+++ packages/kms-tools/renderer/PageLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="layout">
+    <div class="navigation">
+      <a class="navitem" href="/">Home</a>
+      <a class="navitem" href="/about">About</a>
+    </div>
+    <div class="content"><slot /></div>
+  </div>
+</template>
