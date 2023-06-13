da6e4b5cb1ba93decf270b06e17e53b255208bdf
一纸忘忧 <i@ikxin.com>
2023-06-13 22:03:55 +0800

    fix: 修复 dayjs 导出错误

--- packages/kms-tools/src/components/custom-footer.vue
+++ packages/kms-tools/src/components/custom-footer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 
 const currentYear = ref(dayjs().year())
 
@@ -8,22 +8,15 @@ const { t } = useI18n()
 
 <template>
   <ALayoutFooter class="bg-[--color-bg-2] px-2">
-    <div
-      class="mx-auto flex h-14 w-256 max-w-full whitespace-nowrap"
-      flex="~ items-center justify-center"
-    >
+    <div class="mx-auto flex h-14 w-256 max-w-full whitespace-nowrap flex items-center justify-center">
       <ASpace>
         <div>
           Copyright &copy; {{ currentYear }}
-          <ALink target="_blank" href="https://github.com/ikxin/kms-tools">
-            KMS Tools
-          </ALink>
+          <ALink target="_blank" href="https://github.com/ikxin/kms-tools"> KMS Tools </ALink>
         </div>
         <div>
           Code with by
-          <ALink target="_blank" href="https://www.ikxin.com">
-            {{ t('app.author.name') }}
-          </ALink>
+          <ALink target="_blank" href="https://www.ikxin.com">{{ t('app.author.name') }}</ALink>
         </div>
       </ASpace>
     </div>
