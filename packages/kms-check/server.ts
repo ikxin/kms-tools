259990a02fee9d0d2347dbefb41516d7c6be933b
一纸忘忧 <i@ikxin.com>
2023-06-29 15:21:29 +0800

    feat: node http 服务

---
+++ packages/kms-check/server.ts
@@ -0,0 +1,47 @@
+import { createServer } from 'http'
+import { platform } from 'os'
+import { execFile } from 'child_process'
+
+type Body = {
+  port: string
+  host: string
+  app: string
+  protocol: '4' | '5' | '6'
+}
+
+const getResult = (body: Body) => {
+  const { port, host, app, protocol } = body
+
+  return new Promise(resolve => {
+    execFile(
+      `./vlmcs/vlmcs-${platform()}`,
+      [
+        `-${['4', '5', '6'].includes(protocol) ? protocol : '6'}`,
+        `${host}:${port}`,
+        `${app === '' ? '' : '-l ' + app}`
+      ],
+      { timeout: 5000 },
+      (err, std) => {
+        if (err) {
+          resolve({ msg: 'error', result: std.toString() })
+        } else {
+          resolve({ msg: 'success', result: std.toString() })
+        }
+      }
+    )
+  })
+}
+
+const server = createServer(async (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`)
+
+  const body = Object.fromEntries(url.searchParams) as Body
+
+  if (url.pathname === '/check') {
+    res.end(JSON.stringify(await getResult(body)))
+  } else {
+    res.end('KMS Tools')
+  }
+})
+
+server.listen(3000, () => console.log('Server running at http://localhost:3000/'))
