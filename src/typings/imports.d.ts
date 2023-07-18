7793cffbbb0466378cbaa0c3abef2995f5e381e3
一纸忘忧 <i@ikxin.com>
2023-07-18 17:26:04 +0800

    refactor: 更新类型文件

--- src/typings/imports.d.ts
+++ src/typings/imports.d.ts
@@ -193,7 +193,6 @@ declare global {
   const useIntervalFn: typeof import('@vueuse/core')['useIntervalFn']
   const useKeyModifier: typeof import('@vueuse/core')['useKeyModifier']
   const useLastChanged: typeof import('@vueuse/core')['useLastChanged']
-  const useLink: typeof import('vue-router')['useLink']
   const useLocalStorage: typeof import('@vueuse/core')['useLocalStorage']
   const useMagicKeys: typeof import('@vueuse/core')['useMagicKeys']
   const useManualRefHistory: typeof import('@vueuse/core')['useManualRefHistory']
