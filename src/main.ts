1346b4ea2ae8505a87dfe94a0acc816a8b2d8e6a
一纸忘忧 <i@ikxin.com>
2023-07-20 23:50:12 +0800

    style: 自定义主题样式

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { router } from '@/router'
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
-import '@arco-themes/vue-ant-design/css/arco.css'
+import '@/styles/custom.less'
 import 'virtual:uno.css'
 import '@/styles/style.css'
 
