a10b21989c4d4c4c84ea2f1bbb1978dfc3805bbe
一纸忘忧 <i@ikxin.com>
2023-08-14 22:03:14 +0800

    feat: 监控项数据渲染

--- src/store/monitor.ts
+++ src/store/monitor.ts
@@ -1,16 +1,11 @@
 import axios from 'axios'
 import dayjs from 'dayjs'
+import { Notification } from '@arco-design/web-vue'
 
 export const useMonitorStore = defineStore('monitor', () => {
   const apiUrl = 'https://api.uptimerobot.com/v2/getMonitors'
 
-  const apiKey = [
-    'm794366630-4128d6474ac33847941241ec',
-    // 'm794366608-72bc068b4b9b17ef4ed6485d',
-    // 'm794366683-525bb3bcd660d58d54fd8045',
-    // 'm791948435-e8e6c704d43fac76fb6cd3ea',
-    // 'm794992994-7688fbdc23c247ff8f2cd07d',
-  ]
+  const apiKey = ['m794366630-4128d6474ac33847941241ec', 'm794366608-72bc068b4b9b17ef4ed6485d']
 
   const timestamps = Array.from({ length: 120 }, (_, num) => {
     return (
@@ -20,18 +15,25 @@ export const useMonitorStore = defineStore('monitor', () => {
     )
   }).reverse()
 
-  const data = ref([])
+  const monitorData = ref([])
 
-  const getMonitorData = async (api_key: string) => {
-    return await axios.post(apiUrl, {
-      api_key,
-      custom_uptime_ranges: timestamps.join('-'),
+  const getMonitorData = async () => {
+    apiKey.forEach(async item => {
+      try {
+        const {
+          data: { monitors = [] },
+        } = await axios.post(apiUrl, {
+          api_key: item,
+          custom_uptime_ranges: timestamps.join('-'),
+        })
+        monitorData.value.push(...monitors)
+      } catch ({ code, message }) {
+        Notification.error({ title: code, content: message })
+      }
     })
   }
 
-  apiKey.forEach(async item => {
-    data.value.push(await getMonitorData(item))
-  })
+  getMonitorData()
 
-  return { data }
+  return { monitorData }
 })
--- src/views/monitor.vue
+++ src/views/monitor.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { useMonitorStore } from '@/store/monitor'
 
-useMonitorStore()
+const monitorStore = useMonitorStore()
 </script>
 
 <template>
-  <div>
-    <ACard></ACard>
+  <div class="flex flex-col gap-4">
+    <template v-for="item in monitorStore.monitorData" :key="item.id">
+      <ACard :title="item.friendly_name">
+        {{ item.custom_uptime_ranges }}
+      </ACard>
+    </template>
   </div>
 </template>
