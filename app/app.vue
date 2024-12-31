741b6a9b075af102ae5c142e518514212c682101
一纸忘忧 <i@ikxin.com>
2025-01-01 00:28:47 +0800

    feat: add monitoring with endpoint and state management

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+const monitorData = useState('monitorData')
+
+await callOnce(async () => {
+  monitorData.value = await $fetch('/api/monitor')
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
